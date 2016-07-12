929d8a8c2421c4fc7c49bfa80b33ca1d6195c364
Gabriele Romeo <gabriele.romeo@gmail.com>
2016-07-12 13:49:06 +0200

    Initial commit database

---
+++ js/data.js
@@ -0,0 +1,25 @@
+
+var _db = {
+    sections: [
+    {
+        fullName: "",
+        email:    "",
+        color:    ""
+    },
+    {
+        question: "",
+        question: "",
+        question: ""
+    },
+    {
+        question: "",
+        question: "",
+        question: ""
+    },
+    {
+        radio : "",
+        checkbox : [],
+        select : ""
+    }]
+};
+
